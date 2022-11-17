c10e1a7766c038346b6cda1933e1e777b2c0ba5a
irshad-CodeTech <irshad.codegaragetech@gmail.com>
2022-11-17 12:20:00 +0530

    Added functionality like get all data, get element by id,update by id and delete by id

--- index.js
+++ index.js
@@ -36,6 +36,78 @@ app.post('/addblog',async(req,res)=>{
     }
 });
 
+// get all the elements from table 
+app.get('/getinfo',async(req,res)=>{
+    try {
+        
+        const blogdata=await pool.query("SELECT * FROM blogcgt");
+
+        res.json(blogdata.rows);
+        
+    } catch (error) {
+        console.error(error.message);
+        
+    }
+})
+
+// get all the element using id 
+app.get('/getinfo/:id',async(req,res)=>{
+    try {
+        console.log(req.params);
+
+        const {id}=req.params;
+        // console.log(id);
+        // const blogdata=await pool.query(`SELECT * FROM blogcgt WHERE cg_id=2`);
+        const blogdata=await pool.query("SELECT * FROM blogcgt WHERE cg_id=$1",[id]);
+
+        res.json(blogdata.rows);
+        
+    } catch (error) {
+        console.error(error.message);
+        
+    }
+})
+
+// update table using id 
+app.post('/updateinfo',async(req,res)=>{
+    try {
+        console.log(req.body);
+        console.log(req.body.cg_id);
+        console.log(req.body.bloginfo);
+
+
+    const {cg_id,bloginfo}=req.body;
+        
+        const blogdata=await pool.query(
+            "UPDATE blogcgt SET bloginfo=$1 WHERE cg_id=$2",[bloginfo,cg_id]);
+
+        res.json(blogdata.rows);
+        
+    } catch (error) {
+        console.error(error.message);
+        
+    }
+})
+
+// delete table using id 
+
+app.delete('/delinfo/:id',async(req,res)=>{
+    try {
+        console.log(req.params);
+
+        const {id}=req.params;
+        // console.log(id);
+        // const blogdata=await pool.query(`SELECT * FROM blogcgt WHERE cg_id=2`);
+        const blogdata=await pool.query("DELETE FROM blogcgt WHERE cg_id=$1",[id]);
+
+        res.json(blogdata.rows);
+        
+    } catch (error) {
+        console.error(error.message);
+        
+    }
+})
+
 // tell server listen to this port 
 app.listen(port,function(err){
     if(err){
